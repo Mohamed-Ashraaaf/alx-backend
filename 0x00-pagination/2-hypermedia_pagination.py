0901b94610f2f6f9e750624f39778569b99262b5
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-10-23 10:50:38 +0000

    pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -74,7 +74,8 @@ class Server:
         start, end = index_range(page, page_size)
         return dataset[start:end]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Union[int, List[List], None]]:
+    def get_hyper(self, page: int = 1, page_size: int = 10) ->
+    Dict[str, Union[int, List[List], None]]:
         """
         Retrieve and return a hypermedia-formatted page of data.
 
@@ -91,7 +92,8 @@ class Server:
             {'page_size': 2, 'page': 1, 'data': [['2016', 'FEMALE',
                 'ASIAN AND PACIFIC ISLANDER', 'Olivia', '172', '1'],
                                                ['2016', 'FEMALE',
-                                                   'ASIAN AND PACIFIC ISLANDER',
+                                                   'ASIAN
+                                                   AND PACIFIC ISLANDER',
                                                    'Chloe', '112', '2']],
              'next_page': 2, 'prev_page': None, 'total_pages': 9709}
         """
