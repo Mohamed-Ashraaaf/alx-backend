28c9bbf76690b6193dff080adf0261a8512324d3
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-10-25 09:29:45 +0000

    caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
-""" FIFOCache module
+""" LRUCache module
 """
 
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    """ FIFOCache class
+class LRUCache(BaseCaching):
+    """ LRUCache class
     Inherits from BaseCaching and is a caching system
     """
 
     def __init__(self):
-        """ Initialize FIFOCache """
+        """ Initialize LRUCache """
         super().__init__()
-        self.queue = []
+        self.used_keys = []
 
     def put(self, key, item):
         """ Add an item in the cache """
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                discarded = self.queue.pop(0)
-                del self.cache_data[discarded]
-                print("DISCARD: {}".format(discarded))
-            self.queue.append(key)
+                lru_key = self.used_keys.pop(0)
+                del self.cache_data[lru_key]
+                print("DISCARD: {}".format(lru_key))
+            self.used_keys.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key """
         if key is not None and key in self.cache_data:
+            self.used_keys.remove(key)
+            self.used_keys.append(key)
             return self.cache_data[key]
         return None
